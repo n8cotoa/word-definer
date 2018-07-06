adda807064fa4a027e08f822fdcb4feffa52aca5
Nate Cottle <cottle.nate@gmail.com>
2018-07-06 14:05:45 -0700

    Add passing integration test for images.

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -12,6 +12,18 @@ describe('word and definition input', {:type => :feature}) do
     expect(page).to have_content('puppy')
   end
 end
+describe('image link input', {:type => :feature}) do
+  it('process user entry for image and displays image when entry page is open') do
+    visit('/')
+    fill_in('word', :with => 'pizza')
+    fill_in('definition', :with => 'Cheesy Goodness')
+    fill_in('image', :with => 'https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2016/06/shutterstock_225746563-796x531.jpg')
+    click_button('Go!')
+    click_link('pizza')
+    expect(page).to have_xpath("//img[@src='https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2016/06/shutterstock_225746563-796x531.jpg']")
+  end
+end
+
 describe('navigation', {:type => :feature}) do
   it('user click on word on list and displays new page with definition') do
     visit('/')
