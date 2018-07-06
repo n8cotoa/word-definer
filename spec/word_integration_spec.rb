aaaea23f107e814e88f3c7d0643bcdc079648223
Nate Cottle <cottle.nate@gmail.com>
2018-07-06 10:43:50 -0700

    Add second passing integration test.

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -12,3 +12,11 @@ describe('word and definition input', {:type => :feature}) do
     expect(page).to have_content('puppy')
   end
 end
+
+describe('navigation', {:type => :feature}) do
+  it('user click on word on list and displays new page with definition') do
+    visit('/')
+    click_link('puppy')
+    expect(page).to have_content('baby dog')
+  end
+end
